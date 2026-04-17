5980707b591161db6f50046353a706ac57d5d710
Carlos Eduardo Calvio Saavedra <ca.calvio@duocuc.cl>
2026-04-17 10:36:37 -0400

    correcciones para las claves

--- src/ingestion/ingesta_london_crime.py
+++ src/ingestion/ingesta_london_crime.py
@@ -1,27 +1,41 @@
 from google.cloud import bigquery
 import os
+import pandas as pd
+
+BASE_PATH= os.path.join('data', 'bronze')
+os.makedirs(BASE_PATH, exist_ok=True)
+
+file_name= 'london_test.parquet' #nombre especificado para la ingesta de datos
+PATH='.../data/bronze'+file_name #donde iran los datos
 
 #esto configura la runa de almacenamiento visual(ingesta cruda)
-os.makedirs('data/bronze', exist_ok=True)
+os.environ['GOOGLE_APPLICATION_CREDENTIALS']=".../planificacion-london-crime-8b30dd3d4499.json" #se agrega el json de las credenciales
+cred_path=os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
+
 
 def ingesta_bigquery_london():
-    try:
-        client = bigquery.Client()
-
-        query= """
+  if not cred_path or not os.path.exists(cred_path):
+    print("Archivo de credenciales (.json) no encontrado:", cred_path)
+  else:
+    client = bigquery.Client()
+    
+    query= """
             SELECT borough, major_category, minor_category, value, year, month
             FROM `bigquery-public-data.london_crime.crime_by_lsoa`
-            LIMIT 1000
+            LIMIT 10
         """
 
-        df=client.query(query).to_dataframe()
-
-        df.to_csv('data/bronze/london_crime_raw.csv', index=False)
-        print('ingesta de datos completada') 
-
+    df=client.query(query).to_dataframe()
+    
+    try:
+      df.to_parquet(PATH)
+      print(f'datos obtenidos exitosamente en {PATH}')
     except Exception as e:
-        print(f'error de ingesta de datos: {e}') 
+      print(f'directorio no encontrado {PATH}')
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     ingesta_bigquery_london()
+
+
+
